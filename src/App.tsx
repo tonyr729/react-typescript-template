a37549c1a506f2a8e964be2bceeab09f6f62aaf1
Tony Robertson <tony@burndown.io>
2022-03-31 19:52:01 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <ul>
+          <h1 style={{ lineHeight: '10px' }}>Project</h1>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
